2887b73185dc60c744050c95e793543339319a69
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-28 02:23:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,17 +29,16 @@ def initial_round
 end
 
 def hit? (card_total)
-   prompt_user
-   input = get_user_input #get the value from the user input
-   if input == "s"
-      card_total
-   elsif input == "h"
-      card_total += deal_card
-   else
-      invalid_command
-      prompt_user
-   end
-   card_total
+  prompt_user
+  get_user_input
+  if get_user_input == 's'
+    card_total
+  elsif get_user_input == 'h'
+    card_total += deal_card
+  else
+    invalid_command
+    prompt_user
+  end
 end
 
 def invalid_command
