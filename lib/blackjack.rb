5c09accca8ccf5dc1b6670f43bbf2b40ab8ea088
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-28 03:09:34 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,7 +53,7 @@ def runner
   welcome
   current_card_total = initial_round
   until current_card_total > 21
-    hit?(current_card_total)
+    current_card_total = hit?(current_card_total)
     display_card_total(current_card_total)
   end
   end_game(current_card_total)
