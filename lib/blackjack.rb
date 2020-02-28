5a58257e8986f03e11f7af2c9c0b032c7cce300b
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-28 02:46:17 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,8 +35,8 @@ def hit?(current_card_total)
     current_card_total
   elsif user_input == 'h'
     current_card_total += deal_card
-    display_card_total(current_card_total)
-    return current_card_total
+    # display_card_total(current_card_total)
+    # return current_card_total
   else
     invalid_command
     hit?(current_card_total)
