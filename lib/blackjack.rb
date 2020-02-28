c6be571f165f11ef7880a3ae335241739a0c5ef5
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-28 02:40:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,7 +33,6 @@ def hit?(card_total)
   get_user_input
   if get_user_input == 'h'
     card_total += deal_card
-    binding.pry
   elsif get_user_input != 'h' && get_user_input != 's'
     invalid_command
     prompt_user
