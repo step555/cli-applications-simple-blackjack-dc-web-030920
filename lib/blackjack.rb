fe3a20d38091efa44f0c6b3ed39308b497f8b098
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-28 01:58:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -22,8 +22,9 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round(deal_card, display_card_total)
-
+def initial_round
+  card_total = deal_card + deal_card
+  display_card_total
 end
 
 def hit?
