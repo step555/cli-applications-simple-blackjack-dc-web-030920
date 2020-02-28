52bab66ff96c0e9681d0bdca1a6757a7a903a508
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-28 02:19:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,14 +31,7 @@ end
 def hit?(card_total)
   prompt_user
   get_user_input
-  if get_user_input == 'h'
-    card_total += deal_card
-  if get_user_input == 's'
-    card_total
-  else
-    invalid_command
-    end
-  end
+
 end
 
   prompt_user
